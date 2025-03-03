9a622068a4097e885873b661efcd7b4e0b03f852
Степаненко Иван Дмитриевич <dima@it-top.isues>
2025-03-03 13:33:59 +0300

    JMP-12 Add application plugin and configure mainClass

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     jacoco
+    id("application")
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -12,6 +13,10 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
+}
+
+application {
+    mainClass = "com.jmp.userservice.UserMicroserviceApplication"
 }
 
 jacoco {
