93b3cd028803f33fce83e710199c20885db81813
Степаненко Иван Дмитриевич <dima@it-top.isues>
2025-03-12 16:21:26 +0300

    JMP-18 Add @Tag annotation and reorganize import statements in UserController

--- src/main/java/com/jmp/userservice/controller/user/UserController.java
+++ src/main/java/com/jmp/userservice/controller/user/UserController.java
@@ -6,13 +6,22 @@ import com.jmp.userservice.dto.response.UserResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
 
 @RequestMapping("/api/v1/users")
+@Tag(name = "User Controller")
 public interface UserController {
 
     @ApiResponses(value = {
