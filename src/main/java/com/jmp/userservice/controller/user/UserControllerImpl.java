63e495347a282afa655742824fa3875f60d7604e
Степаненко Иван Дмитриевич <dima@it-top.isues>
2025-03-12 16:23:49 +0300

    JMP-18 Add CRUD endpoints to UserControllerImpl

--- src/main/java/com/jmp/userservice/controller/user/UserControllerImpl.java
+++ src/main/java/com/jmp/userservice/controller/user/UserControllerImpl.java
@@ -7,8 +7,14 @@ import com.jmp.userservice.dto.response.UserResponse;
 import com.jmp.userservice.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -18,23 +24,28 @@ import java.util.UUID;
 public class UserControllerImpl implements UserController {
     private final UserService userService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     @Override
-
     public UserResponse createUser(@Valid @RequestBody CreateUserRequest dto) {
         return userService.createUser(dto);
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     @Override
-
     public UserResponse getUserById(@PathVariable UUID id) {
         return userService.getUserById(id);
     }
 
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     @Override
     public UserResponse updateUser(@PathVariable UUID id, @Valid @RequestBody UpdateUserRequest dto) {
-        return userService.updateUser(id, dto);
-    }
+        return userService.updateUser(id, dto);}
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @Override
     public void deleteUser(@PathVariable("id") UUID id) {
         userService.deleteUser(id);
